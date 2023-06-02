8c066c72c82ddd38dc9e237f1549ea722d074ea2
Thais Cristine <this.cristine06@gmail.com>
2023-06-02 17:30:40 +0300

    Adding form elements

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,12 +1,31 @@
-import { Text, View } from 'react-native'
+import { Button, Text, TextInput, Touchable, TouchableOpacity, View } from 'react-native'
 import { styles } from './styles'
-export default function Home(){
+
+export function Home(){
+
+  function handleParticipantAdd(){
+    console.log('yes, yes..')
+  }
   return(
     <View style={styles.container}>
       <Text style={styles.eventName}>
         React Native Event
       </Text>
       <Text style={styles.eventDate}>Friday, May 26th</Text>
+      <View style={styles.form}>
+        <TextInput 
+          style={styles.input} 
+          placeholder='Testing'
+          placeholderTextColor={'#6b6b6b'}
+          keyboardType='email-address'
+        />
+        <TouchableOpacity
+          style={styles.button}
+          onPress={handleParticipantAdd}
+        >
+          <Text style={styles.buttonText}> + </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -14,6 +14,37 @@ export const styles = StyleSheet.create({
     },
     eventDate: {
         color: '#6b6b6b',
-        fontSize: 16
+        fontSize: 16,
+        marginTop: 5
+    },
+    input: {
+        flex: 1,
+        height: 56,
+        backgroundColor: '#1f1e25',
+        borderRadius: 5,
+        color: '#fff',
+        padding: 16,
+        fontSize: 16,
+        // marginTop: 10,
+        marginRight: 12
+    },
+    button :{
+        backgroundColor: '#31cf67',
+        borderRadius: 5,
+        padding: 10,
+        width: 56,
+        height: 56,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    buttonText: {
+        color: '#fff',
+        fontSize: 24
+    },
+    form: {
+        marginTop: 36,
+        marginBottom: 42,
+        width: '100%',
+        flexDirection: 'row'
     }
 })
